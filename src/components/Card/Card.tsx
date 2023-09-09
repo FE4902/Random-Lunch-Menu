bb9364efbef37dc41c7decfc7541e4f3d49cbf05
FE4902 <isdn000123@gmail.com>
2023-09-09 14:09:27 +0900

    Feat: Card click 이벤트

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,6 @@
+import cns from "classnames";
 import S from "./Card.module.scss";
+import { useState } from "react";
 
 type CardType = {
     symbol: string;
@@ -8,9 +10,13 @@ type CardType = {
 
 const Card = (props: CardType): JSX.Element => {
     const { symbol, store, menu } = props;
+    const [isActive, setIsActive] = useState(false);
 
     return (
-        <div className={S.wrap}>
+        <div
+            className={cns(S.wrap, isActive && S.active)}
+            onClick={() => setIsActive(!isActive)}
+        >
             <div className={S.symbol}>{symbol}</div>
             <h4 className={S.store}>{store}</h4>
             <p className={S.menu}>{menu}</p>
